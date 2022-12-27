803f43609048824dc9edbab73385270d1c1473a0
Elys Rivero <elys.rivero@elys.com>
2022-12-27 14:26:27 +0100

    Register hints

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import com.example.demo.dto.ExampleDto;
+import lombok.SneakyThrows;
+import org.postgresql.util.PGobject;
+import org.springframework.aot.hint.ExecutableMode;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(DemoApplication.DemoApplicationRuntimeHints.class)
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+    static class DemoApplicationRuntimeHints implements RuntimeHintsRegistrar {
+
+        @SneakyThrows
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            hints.reflection()
+                    .registerConstructor(PGobject.class.getDeclaredConstructor(), ExecutableMode.INVOKE)
+                    .registerConstructor(ExampleDto.ExampleDtoBuilder.class.getDeclaredConstructor(), ExecutableMode.INVOKE)
+                    .registerMethod(
+                            ExampleDto.ExampleDtoBuilder.class.getMethod("build"), ExecutableMode.INVOKE);
+        }
+    }
 }
